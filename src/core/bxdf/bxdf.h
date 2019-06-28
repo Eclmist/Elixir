0df809d5987c74e2616b9552aeb3431f3be246c9
Samuel Huang <samuel.huang@ubisoft.com>
2019-06-28 17:33:21 +0800

    Added bxdf interface [ci skip]

--- src/core/bxdf/bxdf.h
+++ src/core/bxdf/bxdf.h
@@ -21,38 +21,33 @@
 #pragma once
 
 #include "core/elixir.h"
-#include "core/primitive/transform.h"
 
 exrBEGIN_NAMESPACE
 
-class Scene;
-
-class Light
+class BxDF
 {
 public:
 
-    enum LightFlags
+    enum BxDFType
     {
-        LIGHTFLAGS_DELTAPOSITION  = 1 << 0,
-        LIGHTFLAGS_DELTADIRECTION = 1 << 1,
-        LIGHTFLAGS_AREA           = 1 << 2,
-        LIGHTFLAGS_INFINITE       = 1 << 3,
+        BSDF_REFLECTION   = 1 << 0,
+        BSDF_TRANSMISSION = 1 << 1,
+        BSDF_DIFFUSE      = 1 << 2,
+        BSDF_GLOSSY       = 1 << 3,
+        BSDF_SPECULAR     = 1 << 4,
+        BSDF_ALL          = BSDF_REFLECTION | BSDF_TRANSMISSION | BSDF_DIFFUSE | BSDF_GLOSSY | BSDF_SPECULAR,
     };
 
-    Light(const Transform& transform, exrU32 flags)
-        : m_Transform(transform)
-        , m_Flags(flags) {};
+    BxDF(BxDFType type)
+        : m_BxDFType(type) {};
 
-    inline exrBool IsDeltaLight() { return m_Flags & LightFlags::LIGHTFLAGS_DELTAPOSITION || m_Flags & LightFlags::LIGHTFLAGS_DELTADIRECTION; };
+    exrBool MatchesFlags(BxDFType t) const { return m_BxDFType == t; };
 
-public:
-    virtual void Preprocess(const Scene& scene) {};
-    virtual exrSpectrum SampleLi(const Interaction& interaction, const exrPoint2& uv, exrVector3& wi, exrFloat& pdf) const = 0;
-    virtual exrSpectrum Power() const = 0;
+    virtual exrSpectrum Evaluate(const exrVector3& wo, const exrVector3& wi) const = 0;
+    virtual exrSpectrum EvaluateDelta(const exrVector& wo, exrVector3& wi, BxDFType& thisType) const = 0;
 
-protected:
-    const Transform m_Transform;
-    const exrU32 m_Flags;
+private:
+    BxDFType m_BxDFType;
 };
 
 exrEND_NAMESPACE
