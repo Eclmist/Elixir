0853ee42a1d891f4e3ba4f9920b091c94526daad
Samuel Van Allen <samuel.huanghaoyi@gmail.com>
2020-04-28 08:47:27 +0800

    Refactor systems utils

--- src/system/utils.h
+++ src/system/utils.h
@@ -20,19 +20,42 @@
 
 #pragma once
 
-// To detect multiple namespace opening which might be caused by using namespace elixir usage.
-// if you get an error similar to the one below, it probably means that a using namespace elixir; statement
-// has been found in an include file used by the source file.
-// error C2872: 'inElixirNamespace' : ambiguous symbol
-// To help you find the source, you should activate the display of file inclusion in advanced C++ settings of the project.
 #ifdef EXR_USE_NAMESPACE
-enum { InElixirNamespace = false };
-namespace elixir { enum { InElixirNamespace = true }; }
-#define exrBEGIN_NAMESPACE      exrStaticAssertMsg(!InElixirNamespace, "elixir namespace not previously closed"); namespace elixir {
-#define exrEND_NAMESPACE        } exrStaticAssertMsg(!InElixirNamespace, "elixir namespace not previously closed");
+
+/*
+    Custom BEGIN and END Namespace preprocessor directives. 
+
+    To detect multiple namespace opening which might be caused by using namespace elixir usage.
+    if you get an error similar to the one below, it probably means that a using namespace elixir; statement
+    has been found in an include file used by the source file.
+    error C2872: 'inElixirNamespace' : ambiguous symbol
+    To help you find the source, you should activate the display of file inclusion in advanced C++ settings
+    of the project.
+*/
+
+enum
+{
+    InElixirNamespace = false
+};
+
+namespace elixir 
+{ 
+    enum 
+    { 
+        InElixirNamespace = true 
+    }; 
+}
+
+#define exrBEGIN_NAMESPACE  \
+    exrStaticAssertMsg(!InElixirNamespace, "elixir namespace not previously closed");\
+    namespace elixir {
+
+#define exrEND_NAMESPACE    \
+    } exrStaticAssertMsg(!InElixirNamespace, "elixir namespace not previously closed");
+
 #else
-#define exrBEGIN_NAMESPACE      ((void)0)
-#define exrEND_NAMESPACE        ((void)0)
+#define exrBEGIN_NAMESPACE  ((void)0)
+#define exrEND_NAMESPACE    ((void)0)
 #endif
 
 #include <string.h> // for memcpy
